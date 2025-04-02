f7bc3fcf7e7534c86b5983874838ac0cc0ddae38
Alex Uribe <repzyyog@gmail.com>
2025-04-02 18:14:54 +0200

    Fix repeated code inside every Monument test

--- src/monument/Monument.test.ts
+++ src/monument/Monument.test.ts
@@ -1,22 +1,11 @@
 import Monument from "./Monument.js";
-import { Location } from "./types.js";
 
 describe("Given a monument instance", () => {
   describe("When is instantiated with Hokage Monument", () => {
-    const monumentName = "Hokage Monument";
-    const monumentDescription =
-      "Is the most representative trait of Konohagakure. It is a mountain that has had the faces of all the Hokage carved into the stone.";
-    const monumenImageUrl =
-      "https://static.wikia.nocookie.net/louser/images/6/6b/Hogake_Monument_Naruto_Shippuden.png/revision/latest/scale-to-width-down/250?cb=20130305083411";
-    const monumentLocation: Location = {
-      country: "Land of Fire",
-      city: "Konohagakure",
-    };
-
     test("Then it should return a Monument with name: 'Hokage Monument'", () => {
-      const expectedName = monumentName;
+      const expectedName = "Hokage Monument";
 
-      const hokageMonument = new Monument(monumentName, "", "", {
+      const hokageMonument = new Monument("Hokage Monument", "", "", {
         country: "",
         city: "",
       });
@@ -26,34 +15,46 @@ describe("Given a monument instance", () => {
     });
 
     test("Then it should return a Monument with description: 'Is the most representative trait of Konohagakure. It is a mountain that has had the faces of all the Hokage carved into the stone.'", () => {
-      const expectedDescription = monumentDescription;
+      const expectedDescription =
+        "Is the most representative trait of Konohagakure. It is a mountain that has had the faces of all the Hokage carved into the stone.";
 
-      const hokageMonument = new Monument("", monumentDescription, "", {
-        country: "",
-        city: "",
-      });
+      const hokageMonument = new Monument(
+        "",
+        "Is the most representative trait of Konohagakure. It is a mountain that has had the faces of all the Hokage carved into the stone.",
+        "",
+        {
+          country: "",
+          city: "",
+        },
+      );
       const actualDescription = hokageMonument.description;
 
       expect(actualDescription).toBe(expectedDescription);
     });
 
     test("Then it should return a Monument with an url that opens a image of Hokage Monument", () => {
-      const expectedImageUrl = monumenImageUrl;
+      const expectedImageUrl =
+        "https://static.wikia.nocookie.net/louser/images/6/6b/Hogake_Monument_Naruto_Shippuden.png/revision/latest/scale-to-width-down/250?cb=20130305083411";
 
-      const hokageMonument = new Monument("", "", monumenImageUrl, {
-        country: "",
-        city: "",
-      });
+      const hokageMonument = new Monument(
+        "",
+        "",
+        "https://static.wikia.nocookie.net/louser/images/6/6b/Hogake_Monument_Naruto_Shippuden.png/revision/latest/scale-to-width-down/250?cb=20130305083411",
+        {
+          country: "",
+          city: "",
+        },
+      );
       const actualImageUrl = hokageMonument.imageUrl;
 
       expect(actualImageUrl).toBe(expectedImageUrl);
     });
 
     test("Then it should return a Monument with country: 'Land of Fire'", () => {
-      const expectedCountry = monumentLocation.country;
+      const expectedCountry = "Land of Fire";
 
       const hokageMonument = new Monument("", "", "", {
-        country: monumentLocation.country,
+        country: "Land of Fire",
         city: "",
       });
       const actualCountry = hokageMonument.country;
@@ -62,11 +63,11 @@ describe("Given a monument instance", () => {
     });
 
     test("Then it should return a Monument with city: 'Konohagakure'", () => {
-      const expectedCity = monumentLocation.city;
+      const expectedCity = "Konohagakure";
 
       const hokageMonument = new Monument("", "", "", {
         country: "",
-        city: monumentLocation.city,
+        city: "Konohagakure",
       });
       const actualCity = hokageMonument.city;
 
