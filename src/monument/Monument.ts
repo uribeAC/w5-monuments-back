b42e7d3929da256287e8c37f41d846aeee8d024a
Alex Uribe <repzyyog@gmail.com>
2025-04-02 09:53:34 +0200

    Create Monument class with MonumentStructure type

---
+++ src/monument/Monument.ts
@@ -0,0 +1,21 @@
+import { Location, MonumentStructure } from "./types";
+import { v4 as uuidv4 } from "uuid";
+
+class Monument implements MonumentStructure {
+  public id: string;
+  public country: string;
+  public city: string;
+
+  constructor(
+    public name: string,
+    public description: string,
+    public imageUrl: string,
+    location: Location,
+  ) {
+    this.country = location.country;
+    this.city = location.city;
+    this.id = uuidv4();
+  }
+}
+
+export default Monument;
---
+++ src/monument/types.ts
@@ -0,0 +1,13 @@
+export interface MonumentStructure {
+  id: string;
+  name: string;
+  description: string;
+  imageUrl: string;
+  country: string;
+  city: string;
+}
+
+export type Location = {
+  country: string;
+  city: string;
+};
