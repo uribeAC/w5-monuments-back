6a59b9ac8089b170604a0b49ded78155117a56a3
Alex Uribe <repzyyog@gmail.com>
2025-04-03 12:38:55 +0200

    Create MonumentController class and types

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,5 +1,17 @@
 import express from "express";
+import morgan from "morgan";
+import handleEndpointNotFound from "./middlewares/handleEndpointNotFound.js";
+import MonumentController from "../monument/controller/MonumentController.js";
+import { monuments } from "../monument/data/monuments.js";
 
 const app = express();
+
+app.use(morgan("dev"));
+
+const monumentController = new MonumentController(monuments);
+
+app.get("/monuments", monumentController.getMonuments);
+
+app.use(handleEndpointNotFound);
 
 export default app;
