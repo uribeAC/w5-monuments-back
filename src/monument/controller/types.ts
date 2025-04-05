a4f3a13cca9ebf0ad766766aae3a69927150236b
Alex Uribe <repzyyog@gmail.com>
2025-04-05 20:31:08 +0200

    Create the addMonument method to MonumentController

--- src/monument/controller/types.ts
+++ src/monument/controller/types.ts
@@ -2,4 +2,5 @@ import { Request, Response } from "express";
 
 export interface MonumentControllerStructure {
   getMonuments: (req: Request, res: Response) => void;
+  addMonument: (req: Request, res: Response) => void;
 }
