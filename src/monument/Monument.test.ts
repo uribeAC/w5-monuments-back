f5a92c2ccd38e2fef3b7660b57198b65d765c410
Alex Uribe <repzyyog@gmail.com>
2025-04-06 17:57:26 +0200

    Create POST / monuments endpoint in monumentsRouter

--- src/monument/Monument.test.ts
+++ src/monument/Monument.test.ts
@@ -2,7 +2,7 @@ import Monument from "./Monument.js";
 
 describe("Given a monument instance", () => {
   describe("When is instantiated with Hokage Monument", () => {
-    test("Then it should return a Monument with name: 'Hokage Monument'", () => {
+    test("Then it should have the name: 'Hokage Monument'", () => {
       const expectedName = "Hokage Monument";
 
       const hokageMonument = new Monument("Hokage Monument", "", "", {
@@ -14,7 +14,7 @@ describe("Given a monument instance", () => {
       expect(actualName).toBe(expectedName);
     });
 
-    test("Then it should return a Monument with description: 'Is the most representative trait of Konohagakure. It is a mountain that has had the faces of all the Hokage carved into the stone.'", () => {
+    test("Then it should have the description: 'Is the most representative trait of Konohagakure. It is a mountain that has had the faces of all the Hokage carved into the stone.'", () => {
       const expectedDescription =
         "Is the most representative trait of Konohagakure. It is a mountain that has had the faces of all the Hokage carved into the stone.";
 
@@ -32,7 +32,7 @@ describe("Given a monument instance", () => {
       expect(actualDescription).toBe(expectedDescription);
     });
 
-    test("Then it should return a Monument with an url that opens a image of Hokage Monument", () => {
+    test("Then it should have the url that opens a image of Hokage Monument", () => {
       const expectedImageUrl =
         "https://static.wikia.nocookie.net/louser/images/6/6b/Hogake_Monument_Naruto_Shippuden.png/revision/latest/scale-to-width-down/250?cb=20130305083411";
 
@@ -50,7 +50,7 @@ describe("Given a monument instance", () => {
       expect(actualImageUrl).toBe(expectedImageUrl);
     });
 
-    test("Then it should return a Monument with country: 'Land of Fire'", () => {
+    test("Then it should have the country: 'Land of Fire'", () => {
       const expectedCountry = "Land of Fire";
 
       const hokageMonument = new Monument("", "", "", {
@@ -62,7 +62,7 @@ describe("Given a monument instance", () => {
       expect(actualCountry).toBe(expectedCountry);
     });
 
-    test("Then it should return a Monument with city: 'Konohagakure'", () => {
+    test("Then it should have the city: 'Konohagakure'", () => {
       const expectedCity = "Konohagakure";
 
       const hokageMonument = new Monument("", "", "", {
--- src/monument/router/monumentsRouter.ts
+++ src/monument/router/monumentsRouter.ts
@@ -8,4 +8,6 @@ const monumentController = new MonumentController(monuments);
 
 monumentsRouter.get("/", monumentController.getMonuments);
 
+monumentsRouter.post("/add", monumentController.addMonument);
+
 export default monumentsRouter;
